398166a75585345d8b3b38d56ed6628ca0af44c9
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-04 15:37:54 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 // Use the server.js file to manage our routes
-// Library imports
+// 3rd party library imports
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -14,18 +14,35 @@ app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
   let todo = new Todo({
-      text: req.body.text
+    text: req.body.text
   });
 
-  todo.save().then((doc) => {
+  todo.save().then(
+    doc => {
       res.send(doc);
-  }, e => {
+    },
+    e => {
       res.status(400).send(e);
-  });
+    }
+  );
 });
+
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+// if we just use res.send(todos), we get an array...
+// but ({todos: todos}) gets us an object...
+//to which we can add other properties later
 
 app.listen(3000, () => {
   console.log("Started on port 3000");
 });
 
-module.exports = {app}; // same as app: app
+module.exports = { app }; // same as app: app
