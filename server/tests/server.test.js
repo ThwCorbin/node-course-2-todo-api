6f57b85b3ac53b03aed6f7e1260d6f3a761cfb60
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-04 16:10:57 -0400

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,53 +1,90 @@
 const expect = require("expect");
 const request = require("supertest");
 
-const {app} = require("./../server.js");
-const {Todo} = require("./../models/todo.js");
+const { app } = require("./../server.js");
+const { Todo } = require("./../models/todo.js");
 
-beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+// Create dummy/seed todos for testing
+const todos = [
+  {
+    text: "First test todo"
+  },
+  {
+    text: "Second test todo"
+  }
+];
+
+// This deletes all todoas and then provides seed data (todos) to test
+beforeEach(done => {
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => {
+      done();
+    });
 });
+// results in Error: Timeout of 2000ms exceeded ...
+// until I added Andrew's line below in package.json...
+// mocha --timeout=4000 server/**/*.test.js
+// this may be cause be new version of Expect -- see lecture 104
 
 describe("POST /todos", () => {
-    it("should create a new todo", (done) => {
-        let text = "Test todo text";
+  it("should create a new todo", done => {
+    let text = "Test todo text";
 
-        request(app)
-        .post("/todos")
-        .send({text})
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.text).toBe(text);
-        })
-        .end((err, res) => {
-            if(err) {
-                return done(err);
-            }
+    request(app)
+      .post("/todos")
+      .send({ text })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(1);
-                expect(todos[0].text).toBe(text);
-                done();
-            }).catch(e => done(e));
-        });
-    });
+        Todo.find({ text })
+          .then(todos => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
 
-    it("should not create todo with invalid body data", (done) => {
-        let text = "Test todo text";
+  it("should not create todo with invalid body data", done => {
+    let text = "Test todo text";
 
-        request(app)
-        .post("/todos")
-        .send({})
-        .expect(400)
-        .end((err, res) => {
-            if(err) {
-                return done(err);
-            }
+    request(app)
+      .post("/todos")
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
 
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
-                done();
-            }).catch(e => done(e));
-        });
-    });
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(2);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
+
+describe("GET /todos", () => {
+  it("should get all todos", done => {
+    request(app)
+      .get("/todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
