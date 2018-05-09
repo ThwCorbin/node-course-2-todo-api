35c88fbad31e952c47701c0be7da6e0b1d9613ae
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-09 11:18:47 -0400

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+// $ heroku addons:create mongolab:sandbox
+// $ heroku config
+// provides the dbase link // MONGODB_URI: mongodb://heroku_...
+// Checks if heroku app connects to our dbase || connects to localhost crashes app
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose}; 
 // same as mongoose: mongoose
