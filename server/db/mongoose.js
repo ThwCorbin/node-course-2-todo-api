03a39c27ae7fdeac6a9dffb9900199ea38dad9eb
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-10 13:56:24 -0400

    Fixed errors in configuring env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,8 +5,7 @@ mongoose.Promise = global.Promise;
 // $ heroku addons:create mongolab:sandbox
 // $ heroku config
 // provides the dbase link // MONGODB_URI: mongodb://heroku_...
-// Checks if heroku app connects to our dbase || connects to localhost crashes app
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose}; 
 // same as mongoose: mongoose
