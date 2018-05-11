38b12945343af0fea3c119814f32162db44b2910
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-11 15:36:38 -0400

    Add POST /users route

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,35 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 let User = mongoose.model("User", {
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      minLength: 1
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    unique: true,
+    validate: {
+      // Pass in validator.isEmail method, which returns boolean
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
     }
-  });
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  // MongoDB's token array
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
 
-  module.exports ={User}; // same as User: User
-  
+module.exports = { User }; // same as User: User
